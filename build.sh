c294983bfca3dfb3afb26e5bb83721aad3750bd2
keerthana.v184 <keerthana.v184@gmail.com>
2025-05-17 09:22:04 +0530

    edited

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash 
-echo Hello Keerthana Welcome to Jenkins!
-echo This message is for Email notification check.
+echo "Hello Keerthana Welcome to Jenkins!"
+echo "This message is for Email notification check."
+echo "Email testing"
